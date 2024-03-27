321a698b63f62f20699ede69ffedf0319950e388
LukasHechenbergerID <lukas.hechenberger@insta-drive.com>
2024-03-27 10:55:57 +0100

    installed nuxt

---
+++ app.vue
@@ -0,0 +1,11 @@
+import type { NuxtLink } from '#build/components';
+<template>
+  <div>
+    <div>
+      <NuxtLink to="/" style="margin-right: 1rem">Start Page</NuxtLink>
+
+      <NuxtLink to="/firstPage">Other Page</NuxtLink>
+    </div>
+    <NuxtPage> </NuxtPage>
+  </div>
+</template>
---
+++ pages/firstPage.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Other page</h1>
+</template>
