8861bc05645514acda90c0f0c062f4f8d6bfdf02
LukasHechenbergerID <lukas.hechenberger@insta-drive.com>
2024-03-27 11:14:58 +0100

    added data source

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,10 @@
 <template>
   <h1>Startpage</h1>
+  <p>
+    {{ data }}
+  </p>
 </template>
+
+<script lang="ts" setup>
+const { data } = await useFetch("https://api.apis.guru/v2/list.json");
+</script>
