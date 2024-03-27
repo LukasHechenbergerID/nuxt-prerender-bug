496e66d8c6554cd46a796bc01e0dd43272c6e289
LukasHechenbergerID <lukas.hechenberger@insta-drive.com>
2024-03-27 11:03:50 +0100

    added loading indicator

--- app.vue
+++ app.vue
@@ -1,7 +1,9 @@
-import type { NuxtLink } from '#build/components';
+import type { NuxtLoadingIndicator } from '#build/components'; import type {
+NuxtLink } from '#build/components';
 <template>
   <div>
     <div>
+      <NuxtLoadingIndicator :throttle="0"></NuxtLoadingIndicator>
       <NuxtLink to="/" style="margin-right: 1rem">Start Page</NuxtLink>
 
       <NuxtLink to="/firstPage">Other Page</NuxtLink>
